2d1b510fbab3fa75348e7c61490d04ecbb65904a
Spencer Killen <spleen_387@me.com>
2016-11-12 12:50:45 -0700

    frontend bootsrap

---
+++ client/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("hello word");
+
+class HelloWorld {
+    main() {
+        console.log("hello world");
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const path = require("path"),
+      fs = require("fs");
+
+const webpack = require('webpack'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const BANNER = require("./banner"),
+      PACKAGE = require("./package.json");
+
+module.exports = {
+  entry: "./src/main.js",
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.ts', '.css', 'scss']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel?{presets: "es2015"}',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      },
+    ]
+  },
+
+  plugins: [
+    new webpack.BannerPlugin(BANNER, {entryOnly: true}),
+    new HtmlWebpackPlugin({
+      template: "src/index.ejs",
+      title: PACKAGE.name
+    }),
+    new ExtractTextPlugin("[name].css"),
+    //new webpack.optimize.UglifyJsPlugin(),
+  ]
+};
