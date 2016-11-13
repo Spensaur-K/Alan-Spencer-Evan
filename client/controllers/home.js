3133bfb29987554289bdbd8cd8f38f1ff60ecd2e
EvanGee <evangiese77@gmail.com>
2016-11-12 19:11:58 -0700

    Commit added ayschronus coordinates gathering

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -5,9 +5,36 @@ module.exports = () => {
 
         //clc location
         $(".buy.button").click(e => {
-                order({
-                        type: "coffee",
-                        from: "somehwere (timmies)"
-                });
+
+
+               if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function(position){
+                                var lat = position.coords.latitude;
+                                var long = position.coords.longitude;
+
+                                order({
+                                        type: "coffee",
+                                        from: "somehwere (timmies)",
+                                        latitude: lat,
+                                        longitude: long
+                                });
+
+                        });
+                } else {
+                        // "Geolocation is not supported by this browser.";
+                        var lat = 0;
+                        var long = 0;
+                        order({
+                                type: "coffee",
+                                from: "somehwere (timmies)",
+                                latitude: lat,
+                                longitude: long
+                                });
+
+                }  
+
         });
 };
+
+
+
