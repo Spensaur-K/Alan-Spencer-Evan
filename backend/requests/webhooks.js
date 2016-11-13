66ab070c303e26ce6d626696fa6af048e108b109
Spencer Killen <spleen_387@me.com>
2016-11-12 21:04:51 -0700

    enough to work

--- backend/requests/webhooks.js
+++ backend/requests/webhooks.js
@@ -5,17 +5,10 @@ const webhooks = express.Router();
 
 webhooks.use(bodyParser.urlencoded());
 
-/*webhooks.post("/job_done", (req, res, next) => {
-        req.headers["content-type"] = "application/json";
-        next();
-});*/
-
 webhooks.use(bodyParser.raw());
 
 webhooks.post("/job_change", (req, res, next) => {
 
-        console.log(typeof req.body);
-        console.log(JSON.stringify(req.body));
 });
 
 module.exports = webhooks;
