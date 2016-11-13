d314687bc23e774ae75e4e8759ee0eb9af6618d4
Spencer Killen <spleen_387@me.com>
2016-11-12 21:48:49 -0700

    OOPSIE DOOPSIE

--- backend/requests/webhooks.js
+++ backend/requests/webhooks.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const events = require("events");
+
+
 
 const webhooks = express.Router();
+
+
+module.exports = webhooks;
+
+webhooks.hookEvents = new events.EventEmitter;
 
 webhooks.use(bodyParser.urlencoded());
 
 webhooks.use(bodyParser.raw());
 
-webhooks.post("/job_change", (req, res, next) => {
-        
-});
 
-module.exports = webhooks;
+
+webhooks.post("/job_change", (req, res, next) => {
+        hookEvents.emit("job_change");
+});
